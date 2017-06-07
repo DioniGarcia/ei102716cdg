84bf32de6b4448b7057fae732a9499c3399b0162
flesnuk <flesnuk@gmail.com>
2017-06-07 18:55:27 +0200

    [Login] Store password hash instead of plain-text

--- src/es/uji/ei102716cdg/dao/UserDao.java
+++ src/es/uji/ei102716cdg/dao/UserDao.java
@@ -6,5 +6,5 @@ import es.uji.ei102716cdg.domain.user.User;
 public interface UserDao {
 	User loadUserByUsername(String username, String password);
 	boolean existsUsername(String username);
-	
+	public String encodePassword(String passwd);
 }
